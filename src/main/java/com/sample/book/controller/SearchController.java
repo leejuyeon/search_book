ef354f50cc4a6b07a805f118fe83c026b837e051
DESKTOP-FP9ODTR\juyeon <hico1229>
2019-07-22 13:42:27 +0900

    js example

--- src/main/java/com/sample/book/controller/SearchController.java
+++ src/main/java/com/sample/book/controller/SearchController.java
@@ -2,6 +2,7 @@ package com.sample.book.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,11 +19,11 @@ public class SearchController {
 
 	@RequestMapping(value = "/book", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public BookData searchBook(
-			@RequestParam(required = false, defaultValue="aaa") String userId
+			Authentication authentication
 			, @RequestParam(required = true) String keyword
 			, @RequestParam(required = false, defaultValue="accuracy") String sort
 			, @RequestParam(required = false, defaultValue="1") int page
 			, @RequestParam(required = false, defaultValue="10") int size){
-		return searchBookService.searchBook(userId, keyword, sort, page, size);
+		return searchBookService.searchBook(authentication != null ? authentication.getName() : "", keyword, sort, page, size);
 	}
 }
